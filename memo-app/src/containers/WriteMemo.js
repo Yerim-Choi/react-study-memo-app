6d0f6e228c2ae01b499e9b2c1c81ca65478d3ad8
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-03-03 23:13:39 +0900

    InputSet / SaveButton Component 작업

--- memo-app/src/containers/WriteMemo.js
+++ memo-app/src/containers/WriteMemo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { InputPlaceholder, WhiteBox } from 'components/WriteMemo';
+import { InputSet, SaveButton } from 'components/Shared';
 
 class WriteMemo extends Component {
     render() {
         return (
             <WhiteBox>
-                <InputPlaceholder />
+                {/*<InputPlaceholder/>*/}
+                <InputSet />
+                <SaveButton />
             </WhiteBox>
         );
     }
